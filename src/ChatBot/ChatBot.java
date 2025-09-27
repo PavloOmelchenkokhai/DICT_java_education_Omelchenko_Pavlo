a3dbbd167227dc7626b26635c61913aac934b7e7
PavloOmelchenkoKhai <p.v.omelchenko@student.khai.edu>
2025-09-27 15:17:02 +0300

    ChatBot 2st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,8 +1,14 @@
 package ChatBot;
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! My name is MyChatBot");
         System.out.println("I was created in 2025");
+        System.out.println("Please, remind me your name.");
+        String userName = scanner.nextLine();
+        System.out.println("What a great name you have, " + userName +"!");
+        scanner.close();
     }
 }

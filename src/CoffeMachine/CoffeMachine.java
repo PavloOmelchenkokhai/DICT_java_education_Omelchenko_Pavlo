34d4772d9d050d9ad9c4e72cb93825dba6052fae
PavloOmelchenkoKhai <p.v.omelchenko@student.khai.edu>
2025-10-11 17:53:20 +0300

    CoffeMachine 2st stage

--- src/CoffeMachine/CoffeMachine.java
+++ src/CoffeMachine/CoffeMachine.java
@@ -1,13 +1,35 @@
-package CoffeMachine;
+import java.util.*;
 
 public class CoffeMachine {
+
+    private static final int WATER_PER_CUP_ML = 200;
+    private static final int MILK_PER_CUP_ML = 50;
+    private static final int COFFEE_BEANS_PER_CUP_G = 15;
+
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many cups of coffee you will need:");
+
+        if (scanner.hasNextInt()) {
+            int cups = scanner.nextInt();
+
+            if (cups > 0) {
+                int totalWater = cups * WATER_PER_CUP_ML;
+                int totalMilk = cups * MILK_PER_CUP_ML;
+                int totalCoffeeBeans = cups * COFFEE_BEANS_PER_CUP_G;
+
+                System.out.println("\nFor " + cups + " cups of coffee you will need:");
+                System.out.println(totalWater + " ml of water");
+                System.out.println(totalMilk + " ml of milk");
+                System.out.println(totalCoffeeBeans + " g of coffee beans");
+            } else {
+                System.out.println("Please enter a positive number of cups.");
+            }
+        } else {
+            System.out.println("Invalid input. Please enter a number.");
+        }
+
+        scanner.close();
     }
 }

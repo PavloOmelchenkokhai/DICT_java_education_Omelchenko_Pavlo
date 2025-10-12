c27bc12633977dc69c7338a7a7e6a3bda1f1e13a
PavloOmelchenkoKhai <p.v.omelchenko@student.khai.edu>
2025-10-12 12:16:37 +0300

    CoffeMachine 3st stage

--- src/CoffeMachine/CoffeMachine.java
+++ src/CoffeMachine/CoffeMachine.java
@@ -6,30 +6,46 @@ public class CoffeMachine {
     private static final int MILK_PER_CUP_ML = 50;
     private static final int COFFEE_BEANS_PER_CUP_G = 15;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write how many cups of coffee you will need:");
+            try {
+                System.out.println("Write how many ml of water the coffee machine has:");
+                int availableWater = scanner.nextInt();
 
-        if (scanner.hasNextInt()) {
-            int cups = scanner.nextInt();
+                System.out.println("Write how many ml of milk the coffee machine has:");
+                int availableMilk = scanner.nextInt();
 
-            if (cups > 0) {
-                int totalWater = cups * WATER_PER_CUP_ML;
-                int totalMilk = cups * MILK_PER_CUP_ML;
-                int totalCoffeeBeans = cups * COFFEE_BEANS_PER_CUP_G;
+                System.out.println("Write how many grams of coffee beans the coffee machine has:");
+                int availableBeans = scanner.nextInt();
 
-                System.out.println("\nFor " + cups + " cups of coffee you will need:");
-                System.out.println(totalWater + " ml of water");
-                System.out.println(totalMilk + " ml of milk");
-                System.out.println(totalCoffeeBeans + " g of coffee beans");
-            } else {
-                System.out.println("Please enter a positive number of cups.");
+                System.out.println("Write how many cups of coffee you will need:");
+                int cupsNeeded = scanner.nextInt();
+
+                int maxCupsWater = availableWater / WATER_PER_CUP_ML;
+                int maxCupsMilk = availableMilk / MILK_PER_CUP_ML;
+                int maxCupsBeans = availableBeans / COFFEE_BEANS_PER_CUP_G;
+
+                int maxCups = Math.min(Math.min(maxCupsWater, maxCupsMilk), maxCupsBeans);
+
+                if (maxCups >= cupsNeeded) {
+                    int extraCups = maxCups - cupsNeeded;
+
+                    if (extraCups > 0) {
+                        System.out.println("Yes, I can make that amount of coffee (and even " + extraCups + " more than that)");
+                    } else {
+                        System.out.println("Yes, I can make that amount of coffee");
+                    }
+                } else {
+                    System.out.println("No, I can make only " + maxCups + " cups of coffee");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Invalid input. Please enter a whole number.");
+            } catch (ArithmeticException e) {
+                System.out.println("Error during calculation. Check recipe constants.");
+            } finally {
+                scanner.close();
             }
-        } else {
-            System.out.println("Invalid input. Please enter a number.");
         }
-
-        scanner.close();
     }
-}

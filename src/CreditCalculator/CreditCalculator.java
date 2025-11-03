a900a3519e5512fd834f9454f158d7dbd14acfa9
PavloOmelchenkoKhai <p.v.omelchenko@student.khai.edu>
2025-11-03 16:15:09 +0200

    CreditCalculator 1-st Stage

---
+++ src/CreditCalculator/CreditCalculator.java
@@ -0,0 +1,18 @@
+package CreditCalculator;
+
+public class CreditCalculator {
+    public static void main(String[] args) {
+        int loanPrincipal = 1000;
+        int month1Repayment = 250;
+        int month2Repayment = 250;
+        int month3Repayment = 500;
+
+        System.out.println("Loan principal: " + loanPrincipal);
+
+        System.out.println("Month 1: repaid " + month1Repayment);
+        System.out.println("Month 2: repaid " + month2Repayment);
+        System.out.println("Month 3: repaid " + month3Repayment);
+
+        System.out.println("The loan has been repaid!");
+    }
+}

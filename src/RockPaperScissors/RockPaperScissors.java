4eaf7e06f2fc6754e210ee16c028cce553654640
PavloOmelchenkoKhai <p.v.omelchenko@student.khai.edu>
2025-11-09 13:22:36 +0200

    RockPaperScissors 4-st stage

--- src/RockPaperScissors/RockPaperScissors.java
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -1,10 +1,32 @@
 package RockPaperScissors;
 import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("Hello, " + name);
+
+        int rating = 0;
+        try {
+            File file = new File("rating.txt");
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNext()) {
+                String user = fileScanner.next();
+                int score = fileScanner.nextInt();
+                if (user.equals(name)) {
+                    rating = score;
+                    break;
+                }
+            }
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+        }
 
         String[] options = {"rock", "paper", "scissors"};
 
@@ -16,7 +38,12 @@ public class RockPaperScissors {
                 break;
             }
 
-            if (!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissors")) {
+            if (userChoice.equals("!rating")) {
+                System.out.println("Your rating: " + rating);
+                continue;
+            }
+
+            if (!Arrays.asList(options).contains(userChoice)) {
                 System.out.println("Invalid input");
                 continue;
             }
@@ -25,12 +52,14 @@ public class RockPaperScissors {
 
             if (userChoice.equals(computerChoice)) {
                 System.out.println("There is a draw (" + computerChoice + ")");
+                rating += 50;
             } else if (
                     (userChoice.equals("rock") && computerChoice.equals("scissors")) ||
                             (userChoice.equals("scissors") && computerChoice.equals("paper")) ||
                             (userChoice.equals("paper") && computerChoice.equals("rock"))
             ) {
                 System.out.println("Well done. The computer chose " + computerChoice + " and failed");
+                rating += 100;
             } else {
                 System.out.println("Sorry, but the computer chose " + computerChoice);
             }

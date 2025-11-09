1c7b9787bf2326ae0e3ba4e938a8c1e07c1a4731
PavloOmelchenkoKhai <p.v.omelchenko@student.khai.edu>
2025-11-09 13:15:55 +0200

    RockPaperScissors 2-st stage

--- src/RockPaperScissors/RockPaperScissors.java
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -4,26 +4,30 @@ import java.util.*;
 public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
         String userChoice = scanner.nextLine().trim().toLowerCase();
 
-        String computerChoice;
+        String[] options = {"rock", "paper", "scissors"};
 
-        switch (userChoice) {
-            case "rock":
-                computerChoice = "paper";
-                break;
-            case "paper":
-                computerChoice = "scissors";
-                break;
-            case "scissors":
-                computerChoice = "rock";
-                break;
-            default:
-                System.out.println("Invalid input");
-                return;
+        String computerChoice = options[random.nextInt(options.length)];
+
+        if (userChoice.equals(computerChoice)) {
+            System.out.println("There is a draw (" + computerChoice + ")");
+        } else if (
+                (userChoice.equals("rock") && computerChoice.equals("scissors")) ||
+                        (userChoice.equals("scissors") && computerChoice.equals("paper")) ||
+                        (userChoice.equals("paper") && computerChoice.equals("rock"))
+        ) {
+            System.out.println("Well done. The computer chose " + computerChoice + " and failed");
+        } else if (
+                userChoice.equals("rock") ||
+                        userChoice.equals("paper") ||
+                        userChoice.equals("scissors")
+        ) {
+            System.out.println("Sorry, but the computer chose " + computerChoice);
+        } else {
+            System.out.println("Invalid input");
         }
-
-        System.out.println("Sorry, but the computer chose " + computerChoice);
     }
-
 }
